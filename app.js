176e4755fe3e872075a2da347f27bf7cde44f36b
Beatrizi <beatrici25@gmail.com>
2022-06-10 16:41:07 +0100

    fully working the eventlistener to do a matematical operation

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-console.log("caca")
-let numUno = document.getElementById("numeroUno").value;
-let numDos = document.getElementById("numerodos").value;
-let boton = document.querySelector("#operacion");
-let escribir = document.getElementById('almacenarAqui');
-//enviar valores de html  a js .value
+let numUno= document.getElementById("numeroUno")
 
-function resultadoOperacion(datoUno, datoDos) {
-    let numUno= datoUno;
-    let numDos= datoDos;
-  let resultadoOperacion = numUno + numDos;
-  escribir.textContent = resultadoOperacion;
+let numDos = document.getElementById('numeroDos')
 
+let boton = document.getElementById("operacion")
+let escribir =  document.getElementById("almacenarAqui")
+
+function Operacion(num1, num2) {
+     return num1+num2
 }
 
-let ponerDatos = boton.addEventListener("click",resultadoOperacion(numUno,numDos))
+//poner datos al click
+boton.addEventListener("click",resultadoOperacion)
+
+
+// boton.addEventListener("click", resultadoOperacion(numUno.value,numDos.value))
+
+function resultadoOperacion() {
+  console.log("sigoIntentando")
+  let inputValuenumOne = parseInt(numUno.value)
+  let inputValuenumTwo = parseInt(numDos.value)
+  let resultado = Operacion(inputValuenumOne,inputValuenumTwo)
+
+  escribir.value = resultado;
+  console.log(escribir.value)
+
+  // escribir.innerHTML = resultadoOperacion(numUno.value,numDos.value);
+  // console.log(  document.getElementById("almacenarAqui").value = Date()
+  // )
+}
+
+
+
+}

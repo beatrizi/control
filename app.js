14b112e899d12cf2139e7e5b998415f8252d9c9f
Beatrizi <beatrici25@gmail.com>
2022-06-10 21:20:56 +0100

    eventlistener coger el mensaje de la caja y ponerlo en otro sitio usando value dentro funcion

--- app.js
+++ app.js
@@ -12,9 +12,6 @@ function Operacion(num1, num2) {
 //poner datos al click
 boton.addEventListener("click",resultadoOperacion)
 
-
-// boton.addEventListener("click", resultadoOperacion(numUno.value,numDos.value))
-
 function resultadoOperacion() {
   console.log("sigoIntentando")
   let inputValuenumOne = parseInt(numUno.value)
@@ -23,12 +20,27 @@ function resultadoOperacion() {
 
   escribir.value = resultado;
   console.log(escribir.value)
+}
+/*copiando texto de caja y mostrado en una seccion lo que muestra el texto */
+let inputValue = document.getElementById("inputTexto");
+console.log("el valor recogido antes entrar en la funcion es:"+typeof inputValue )
+let botonClickMe = document.getElementById("buttonText");
+let ActulizandoWriting = document.getElementById("writeValueInput");
 
-  // escribir.innerHTML = resultadoOperacion(numUno.value,numDos.value);
-  // console.log(  document.getElementById("almacenarAqui").value = Date()
-  // )
+/**Reconociendo el click */
+botonClickMe.addEventListener('click', actualizarEscritura)
+
+function actualizarEscritura(){
+  let valorRecogido = inputValue.value;  //dentro de la f(x) value, si declaro no la muestra
+  console.log(typeof valorRecogido)//string
+  console.log(valorRecogido)//imprime y funciona
+
+  ActulizandoWriting.innerHTML = valorRecogido; 
 }
 
 
-
-}
+/**error */
+// const actualizarEscritura=()=>{
+//   let valorRecogido = inputValue.value; 
+//   ActulizandoWriting.innerHTML = valorRecogido; 
+// } //Uncaught ReferenceError: Cannot access 'actualizarEscritura' before initialization  at app.js:31:40
